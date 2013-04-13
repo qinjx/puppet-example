f7285dc18ab3626cd59ce73b7fe973e3026060f4
qinjx <talkativedoggy@gmail.com>
2013-04-14 00:01:58 +0800

    安装puppet服务端和客户端的代码OK

--- install_scripts/func.inc.sh
+++ install_scripts/func.inc.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 function test_url() {
-    echo test
+	local url=$1
+	if curl --output /dev/null --silent --head --fail ${url}; then
+		echo OK
+	else
+		echo FAILED
+	fi
 }
 
 function prepare_yum_repo() {
-    echo yum
+	local yum_mirror_prefix="http://mirrors.sohu.com/fedora-epel/"
+	if [[ "OK" = $(test_url ${yum_mirror_prefix}) ]]; then
+		echo "[epel]
+name=CentOS-\$releasever - EPEL
+baseurl=${yum_mirror_prefix}\$releasever/\$basearch/
+enabled=1
+gpgcheck=1
+gpgkey=${yum_mirror_prefix}RPM-GPG-KEY-EPEL-\$releasever
+" > /etc/yum.repos.d/epel.repo
+		echo OK
+	else
+		echo FAILED
+	fi
 }
 
 function install_master(){
-    echo master
+	echo install master
+	yum install -y puppet-server
 }
 
 function install_client() {
-    echo client
+	echo client
+	yum install -y puppet
 }
--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -4,13 +4,16 @@
 echo "Please choose the role of this machine"
 options="Puppet-Master  Puppet-Client"
 select opt in ${options}; do
-    if [ "${opt}" = "Puppet-Master" ]; then
-        install_master;
-        exit
-    elif [ "${opt}" = "Puppet-Client" ]; then
-        install_client;
-        exit
-    else
-        echo "Bad option, please choose again"
-    fi
+	if [[ "${opt}" = "Puppet-Master" || "${opt}" = "Puppet-Client" ]]; then
+		prepare_yum_repo || exit
+		if [ "${opt}" = "Puppet-Master" ]; then
+			install_master;
+			exit
+		elif [ "${opt}" = "Puppet-Client" ]; then
+			install_client;
+			exit
+		fi
+	else
+		echo "Bad option, please choose again"
+	fi
 done
