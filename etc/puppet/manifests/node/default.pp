b6f38a38c7e2ce9af451f08ece9c504355f3451f
qinjx <root@puppet-server.vip.example.com>
2013-05-07 01:32:57 +0800

    加入puppet_client，测试成功

--- etc/puppet/manifests/node/default.pp
+++ etc/puppet/manifests/node/default.pp
@@ -1,6 +1,3 @@
 node default {
-	notify {
-		"warning_from_default_node":
-			name => "If you can see this warning, that means your node didn't match any node definition";
-	}
+	include role_puppet_client
 }
--- etc/puppet/manifests/node/puppet.client.pp
+++ etc/puppet/manifests/node/puppet.client.pp
@@ -1,4 +1,4 @@
-node /^puppet\-client/ {
+node /^puppet\-client/ inherits default {
 	include fast_yum_repo
 	include percona_yum_repo
 }
