92eb20a260e23c963b715fad146eefb43b0ba63b
qinjx <root@puppet-server.vip.example.com>
2013-05-08 07:04:36 +0800

      file barcket测试成功

--- etc/puppet/manifests/node/default.pp
+++ etc/puppet/manifests/node/default.pp
@@ -9,6 +9,7 @@ node default {
 			require => File["/root/init_scripts/"];
 		"/root/init_scripts/init.sh":
 			ensure => present,
+			source => "puppet:///files/node/default/init.sh",
 			require => File["/root/init_scripts/conf.ini"];
 	}
 
