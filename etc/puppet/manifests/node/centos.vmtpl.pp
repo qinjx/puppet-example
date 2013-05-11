5ec2a12a309b02d56aaf959569c44e54891df28d
qinjx <talkativedoggy@gmail.com>
2013-05-11 22:27:07 +0800

    centos vmtpl使用yum::repo，node继承default

--- etc/puppet/manifests/node/centos.vmtpl.pp
+++ etc/puppet/manifests/node/centos.vmtpl.pp
@@ -1,3 +1,3 @@
-node /^centos\d*\.vmtpl/ {
+node /^centos\d*\.vmtpl/ inherits default {
 	include role_centos_vm_tpl
 }
--- etc/puppet/manifests/role/centos_vm_tpl.pp
+++ etc/puppet/manifests/role/centos_vm_tpl.pp
@@ -1,5 +1,5 @@
 class role_centos_vm_tpl {
-	include fast_yum_repo,  base_yum_repo
+	include yum::repo::base
 
 	file {
 		"/etc/udev/rules.d/70-persistent-net.rules":
@@ -21,6 +21,6 @@ class role_centos_vm_tpl {
 	package {
 		["vim-enhanced", "rsync", "cronie"]:
 			ensure => present,
-			require => Class["base_yum_repo"],
+			require => Class["yum::repo::base"],
 	}
 }
