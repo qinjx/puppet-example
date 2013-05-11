68abf910ecad5001072a629bfc77d8dcacfd93b9
qinjx <talkativedoggy@gmail.com>
2013-05-11 21:54:09 +0800

    puppet_client改为puppet::client

---
+++ etc/puppet/modules/puppet/manifests/client/conf.pp
@@ -0,0 +1,6 @@
+define puppet::client::conf($ensure) {
+	augeas {
+		"puppet.conf_${name}" :
+			changes => "set /files/etc/puppet/puppet.conf/${name} ${ensure}";
+	}
+}
---
+++ etc/puppet/modules/puppet/manifests/client/service.pp
@@ -0,0 +1,12 @@
+class puppet::client::service {
+	package {
+		"puppet":
+			ensure => installed;
+	}
+
+	service {
+		"puppet":
+			enable => true,
+			ensure => running,
+	}
+}
