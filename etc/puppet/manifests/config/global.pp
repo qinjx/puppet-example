1ea9eea2a3369c669def5f1d8b03a3f9ca7979c9
qinjx <talkativedoggy@gmail.com>
2013-05-15 16:28:12 +0800

    调整IP前缀

--- etc/puppet/manifests/config/global.pp
+++ etc/puppet/manifests/config/global.pp
@@ -1,10 +1,10 @@
 class config::global {
-	$ip_prefix = "172.16.0"
+	$ip_prefix = "10.250.0"
 	
 	$root_domain_name = "example.com"
 	$host_puppet_server = "puppet-server.vip.$root_domain_name"
 	
-	$local_yum_mirror_prefix = "172.16.0.5/"
+	$local_yum_mirror_prefix = "yum-mirror.vip.$root_domain_name/"
 	
 	$url_yum_mirror_base = "http://${local_yum_mirror_prefix}mirrors.sohu.com"
 	$url_yum_mirror_epel = "http://${local_yum_mirror_prefix}mirrors.sohu.com"
