7b661bea4b8a0c2b0e71a12dba2a9bb676845233
qinjx <talkativedoggy@gmail.com>
2013-05-16 18:30:42 +0800

    增加puppet::master::service

---
+++ etc/puppet/modules/puppet/manifests/master/service.pp
@@ -0,0 +1,29 @@
+class puppet::master::service
+{
+	package
+	{
+		["puppet-server"]:
+		ensure => present,
+		require => Yumrepo["epel"],
+	}
+
+	service
+	{
+		"puppetmaster":
+			ensure     => running,
+			enable     => true,
+			require    => Package["puppet-server"],
+	}
+
+	iptables
+	{
+		"puppetmaster_8140":
+			proto       => "tcp",
+			dport       => "8140",
+			jump        => "ACCEPT";
+		"puppetmaster_8140_udp":
+			proto       => "udp",
+			dport       => "8140",
+			jump        => "ACCEPT";
+	}
+}
