002fc441c1b81c70ca1377b9b3a3f8692ed78751
qinjx <talkativedoggy@gmail.com>
2013-05-17 16:48:51 +0800

    改一上augeas写法

--- etc/puppet/modules/puppet/manifests/client/conf.pp
+++ etc/puppet/modules/puppet/manifests/client/conf.pp
@@ -1,6 +1,7 @@
 define puppet::client::conf($ensure) {
 	augeas {
 		"puppet.conf_${name}" :
-			changes => "set /files/etc/puppet/puppet.conf/${name} ${ensure}";
+			content => "/files/etc/puppet/puppet.conf",
+			changes => "set ${name} ${ensure}";
 	}
 }
