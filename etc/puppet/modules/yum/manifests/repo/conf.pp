84952694db9b4b67ed33e98f5b3b40a4f47a842e
qinjx <talkativedoggy@gmail.com>
2013-05-18 15:28:52 +0800

    yum::repo::conf bugfix，baseurl加http前缀

--- etc/puppet/modules/yum/manifests/repo/conf.pp
+++ etc/puppet/modules/yum/manifests/repo/conf.pp
@@ -1,4 +1,10 @@
 define yum::repo::conf {
+	if ($config::yum::use_local_yum_mirror) {
+		$url_prefix = "http://$config::yum::local_yum_mirror_prefix/"
+	} else {
+		$url_prefix = "http://"
+	}
+
 	yumrepo {
 		$name:
 		baseurl => $config::yum::urls[$name],
