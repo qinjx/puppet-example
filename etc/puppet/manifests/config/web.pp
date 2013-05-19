18123179d085e08bc2eb5d6d32d779e5f9ef844c
qinjx <talkativedoggy@gmail.com>
2013-05-19 16:33:34 +0800

    web配置，未测试

---
+++ etc/puppet/manifests/config/web.pp
@@ -0,0 +1,4 @@
+class config::web {
+	$public_domain_name = "wuzhuangbu.net"
+	$www_root = "/var/www/lotusec/web_entrance"
+}
--- etc/puppet/manifests/node/nginx.web.pp
+++ etc/puppet/manifests/node/nginx.web.pp
@@ -1,3 +1,10 @@
 node /^nginx\d*\.web/ inherits default {
+	web::server::site {
+		"www.${config::web::public_domain_name}":
+		    www_root => "${config::web::www_root}/www/",
+		    fcgi => "php.app.vip",
+	}
+
 	include role_web_server
+	include config::web
 }
