f15b7502b266f0df9d49203efc1cf10be131bea7
qinjx <talkativedoggy@gmail.com>
2013-05-22 06:01:46 +0800

    php::conf tested

--- etc/puppet/manifests/role/php_app_server.pp
+++ etc/puppet/manifests/role/php_app_server.pp
@@ -1,3 +1,11 @@
 class role_php_app_server {
+
+	php::conf {
+		"Date/date.timezone":
+			ensure => "Asia/Chongqing";
+		"PHP/display_errors":
+			ensure => "On";
+	}
+
 	include php::install, php::service
 }
--- etc/puppet/modules/php/manifests/conf.pp
+++ etc/puppet/modules/php/manifests/conf.pp
@@ -1,6 +1,7 @@
 define php::conf($ensure) {
 	augeas {
 		"change_php_setting_${name}":
-		changes => "set /files/etc/php.ini/target[. = 'mysqld']/${name} ${ensure}",
+			context => "/files/etc/php.ini",
+			changes => "set ${name} ${ensure}",
 	}
 }
