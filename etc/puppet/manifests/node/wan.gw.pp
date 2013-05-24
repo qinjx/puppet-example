1b3c8b3c01d003990faa752e1c3e9622bcbafb29
qinjx <talkativedoggy@gmail.com>
2013-05-24 23:06:03 +0800

    端口转发规则构造，没测试

--- etc/puppet/manifests/node/wan.gw.pp
+++ etc/puppet/manifests/node/wan.gw.pp
@@ -5,5 +5,15 @@ node /^wan\d*\.gw/ inherits default {
 			wan_interface => "eth1",
 	}
 
+	router::port::forward {
+		"tcp_80":
+		    wan_interface => "eth1",
+		    proto => "tcp",
+		    port => 80,
+		    ip => $config::wan::ip,
+		    dest_ip => $config::hosts::ip_list[user_wan_lb_vip],
+			dest_port => 80
+	}
+
 	include role_vip_holder, role_load_balancer, role_gateway
 }
--- etc/puppet/modules/router/manifests/port/forward.pp
+++ etc/puppet/modules/router/manifests/port/forward.pp
@@ -1,2 +1,13 @@
-class router::port::forward {
+define router::port::forward($wan_interface, $proto, $ip, $port, $dest_ip, $dest_port) {
+	iptables {
+		"port_forward_rule_for_${proto}_${port}":
+			table => "nat",
+		    chain => "PREROUTING",
+			iniface => $wan_interface,
+		    proto => $proto,
+		    dport => $port,
+			destination	=> $ip,
+			todest	=> "$dest_ip:$dest_port",
+		    jump => "DNAT",
+	}
 }
