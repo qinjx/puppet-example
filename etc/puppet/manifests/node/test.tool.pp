a5a514a9b22c8195ce015ff33a60f111cd04c50a
qinjx <talkativedoggy@gmail.com>
2013-05-24 18:18:10 +0800

    role_internet_enduser中写死的网关IP改为配置变量，测试通过

---
+++ etc/puppet/manifests/node/test.tool.pp
@@ -0,0 +1,3 @@
+node /^test\d*\.tool/ inherits default {
+	include role_internet_enduser
+}
--- etc/puppet/manifests/role/internet_enduser.pp
+++ etc/puppet/manifests/role/internet_enduser.pp
@@ -1,9 +1,11 @@
 class role_internet_enduser {
+	$wan_gw_vip = $config::hosts::ip_list[wan_gw_vip]
+
 	augeas {
 		"set_gateway":
 			context => "/files/etc/sysconfig/network",
 			changes => [
-				"set GATEWAY ${config::global::ip_prefix}.254"
+				"set GATEWAY ${config::global::ip_prefix}.$wan_gw_vip"
 			];
 
 		"set_dns":
