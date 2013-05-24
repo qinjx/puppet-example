8127e740f5c4d3836d4c1f25c59c02388f62f04a
qinjx <talkativedoggy@gmail.com>
2013-05-25 07:47:16 +0800

    port forward模块可生成iptables rule，但还没测试是否正常转发

--- etc/puppet/manifests/node/wan.gw.pp
+++ etc/puppet/manifests/node/wan.gw.pp
@@ -7,12 +7,12 @@ node /^wan\d*\.gw/ inherits default {
 
 	router::port::forward {
 		"tcp_80":
-		    wan_interface => "eth1",
-		    proto => "tcp",
-		    port => 80,
-		    ip => $config::wan::ip,
-		    dest_ip => $config::hosts::ip_list[user_wan_lb_vip],
-			dest_port => 80
+			wan_interface => "eth1",
+			proto => "tcp",
+			ip => $config::global::wan_ip,
+			port => 80,
+			dest_ip => "${config::global::ip_prefix}.${config::hosts::ip_list[user_wan_lb_vip]}",
+			dest_port => 80,
 	}
 
 	include role_vip_holder, role_load_balancer, role_gateway
--- etc/puppet/modules/router/manifests/port/forward.pp
+++ etc/puppet/modules/router/manifests/port/forward.pp
@@ -2,12 +2,12 @@ define router::port::forward($wan_interface, $proto, $ip, $port, $dest_ip, $dest
 	iptables {
 		"port_forward_rule_for_${proto}_${port}":
 			table => "nat",
-		    chain => "PREROUTING",
+			chain => "PREROUTING",
 			iniface => $wan_interface,
-		    proto => $proto,
-		    dport => $port,
-			destination	=> $ip,
-			todest	=> "$dest_ip:$dest_port",
-		    jump => "DNAT",
+			proto => $proto,
+			dport => $port,
+			destination => $ip,
+			todest => "$dest_ip:$dest_port",
+			jump => "DNAT",
 	}
 }
