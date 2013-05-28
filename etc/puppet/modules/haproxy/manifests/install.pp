fabe456af17043d36df663466c22471a06dab43b
qinjx <talkativedoggy@gmail.com>
2013-05-29 04:27:34 +0800

    haproxy多配置文件支持，init脚本已改haproxy还没测

--- etc/puppet/modules/haproxy/manifests/install.pp
+++ etc/puppet/modules/haproxy/manifests/install.pp
@@ -2,6 +2,24 @@ class haproxy::install {
 	package {
 		"haproxy":
 			ensure => installed,
-		    require => Yum::Repo::Conf["epel"],
+			require => Yum::Repo::Conf["epel"],
+	}
+
+	file {
+		"/etc/haproxy/conf.d":
+			ensure => directory,
+			require => Package["haproxy"],
+	}
+
+	exec {
+		"inert_conf_array_line":
+			command => 'sed -i "/prog=/ a conf_array=\`ls /etc/haproxy/conf.d\`\
+conf_array+=\(\/etc\/\$prog\/\$prog\.cfg)" /etc/init.d/haproxy',
+			unless => "grep conf_array /etc/init.d/haproxy 2>/dev/null",
+			path => "/bin";
+
+		"enable_multi_config_file_for_haproxy":
+			command => 'sed -i "s/\/etc\/\$prog\/\$prog\.cfg/\$\{conf_array\[@\]\}/g" /etc/init.d/haproxy',
+			path => "/bin";
 	}
 }
