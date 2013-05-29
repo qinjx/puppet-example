1986e178be07bf6cc03ac07ad48c7b00dbdbbd9f
qinjx <talkativedoggy@gmail.com>
2013-05-30 03:40:09 +0800

    haproxy init.d脚本测试成功

--- etc/puppet/modules/haproxy/manifests/install.pp
+++ etc/puppet/modules/haproxy/manifests/install.pp
@@ -13,14 +13,13 @@ class haproxy::install {
 
 	exec {#多个配置文件要指定多次-f参数
 		"inert_conf_array_line":
-			command => 'sed -i "/prog=/ a conf_array=\`find /etc/haproxy/conf.d -name *.conf\`\
-conf_array+=\(\/etc\/\$prog\/\$prog\.cfg)" /etc/init.d/haproxy',
+			command => 'sed -i "/prog=/ a conf_array=\`find /etc/haproxy -name \'*.cfg\' | sort -r | awk \'{print \"-f \", \$1}\'\`" /etc/init.d/haproxy',
 			unless => "grep conf_array= /etc/init.d/haproxy 2>/dev/null",
 			path => "/bin",
 			require => Package["haproxy"];
 
 		"enable_multi_config_file_for_haproxy":
-			command => 'sed -i "s/\/etc\/\$prog\/\$prog\.cfg/\$\{conf_array\[@\]\}/g" /etc/init.d/haproxy',
+			command => 'sed -i "s/\-f \/etc\/\$prog\/\$prog\.cfg/\$\{conf_array\[@\]\}/g" /etc/init.d/haproxy',
 			path => "/bin",
 			require => Package["haproxy"];
 	}
