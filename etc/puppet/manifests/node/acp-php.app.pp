0f0965cd750db352ee24ca4ad2cf80882b1e42e6
qinjx <talkativedoggy@gmail.com>
2013-06-01 11:24:10 +0800

    acp-php.app监听9001端口，为了不和lan.lb上的php.app冲突

--- etc/puppet/manifests/node/acp-php.app.pp
+++ etc/puppet/manifests/node/acp-php.app.pp
@@ -1,3 +1,20 @@
 node /^acp-php\d*\.app/ inherits default {
-	include role_centos_vmtpl
+	php::conf {
+		"PHP/max_input_vars":
+			ensure => "10000",
+	}
+
+	php::ext {
+		["exif"]:
+		    ensure => installed,
+	}
+
+	php::fpm::conf {
+		"listen":
+			pool => "www",
+			section => "www",
+			ensure => "0.0.0.0:9001",
+	}
+
+	include role_php_app_server
 }
