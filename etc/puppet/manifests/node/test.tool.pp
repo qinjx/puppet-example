106810fbba7f00e1adc3e23ecc0d32d29b71aa1b
qinjx <talkativedoggy@gmail.com>
2013-06-12 23:19:24 +0800

    compiler script

--- etc/puppet/manifests/node/test.tool.pp
+++ etc/puppet/manifests/node/test.tool.pp
@@ -1,3 +1,14 @@
 node /^test\d*\.tool/ inherits default {
+	file {
+		"/opt/src":
+			ensure => directory;
+
+		"/opt/src/fastdfs_auto_compiler.sh":
+			source => "puppet:///files/node/test.tool/fastdfs_auto_compiler.sh";
+
+		"/opt/src/php_fdfs_auto_compiler.sh":
+			source => "puppet:///files/node/test.tool/php_fdfs_auto_compiler.sh";
+	}
+
 	include role_internet_enduser, role_compiler
 }
