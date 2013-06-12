85fd4fde17bc9b6342e6ba7bfde288715069cec9
qinjx <talkativedoggy@gmail.com>
2013-06-12 15:44:36 +0800

    去掉点

--- etc/puppet/manifests/node/vnc.sys.pp
+++ etc/puppet/manifests/node/vnc.sys.pp
@@ -1,8 +1,8 @@
 node /^vnc\d*\.sys/ inherits default {
-	$ip_suffix = regsubst($ipaddress, $config::global::ip_prefix, "", "G")
+	$ip_suffix = regsubst($ipaddress, "${config::global::ip_prefix}.", "", "G")
 	keepalived::vrrp::instance {
 		"$ip_suffix":
-		vip_add => "${config::global::phy_server_ip_prefix}$ip_suffix/24",
+		vip_add => "${config::global::phy_server_ip_prefix}.$ip_suffix/24",
 	}
 
 	package {
