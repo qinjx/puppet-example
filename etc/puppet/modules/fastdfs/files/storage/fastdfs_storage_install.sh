3a6febbac1a420833250f7b920c721131b72c6e8
qinjx <talkativedoggy@gmail.com>
2013-06-15 23:52:09 +0800

    client从storage脚本中删除

--- etc/puppet/modules/fastdfs/files/storage/fastdfs_storage_install.sh
+++ etc/puppet/modules/fastdfs/files/storage/fastdfs_storage_install.sh
@@ -8,7 +8,7 @@ ln -s /usr/lib64/libfastcommon.so.1 /usr/lib64/libfastcommon.so
 ln -s /usr/lib64/libfdfsclient.so.1 /usr/lib64/libfdfsclient.so
 
 #config storage
-mkdir -p /mnt/fastdfs/{storage,client,upload_files}
+mkdir -p /mnt/fastdfs/{storage,upload_files}
 
 ############## /etc/fdfs/storage.conf ##############
 sed -i -e 's/base_path=.*/base_path=\/mnt\/fastdfs\/storage/' /etc/fdfs/storage.conf
@@ -18,8 +18,3 @@ sed -i -e '/^tracker_server=/d' /etc/fdfs/storage.conf
 ############## please make sure ALL tracker server are online ##############
 echo "tracker_server=tracker1.dfs:22122
 tracker_server=tracker2.dfs:22122" >> /etc/fdfs/storage.conf
-
-############## /etc/fdfs/client.conf ##############
-sed -i -e 's/base_path=.*/base_path=\/mnt\/fastdfs\/client/' /etc/fdfs/client.conf
-sed -i -e '/^tracker_server=/d' /etc/fdfs/client.conf
-echo "tracker_server=tracker.dfs.vip:22122" >> /etc/fdfs/client.conf
