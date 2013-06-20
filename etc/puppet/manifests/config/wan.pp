e981cf39a4609a23f8f0e12dc9831072ba364d77
qinjx <talkativedoggy@gmail.com>
2013-06-20 09:50:40 +0800

    ip and domain changes

--- etc/puppet/manifests/config/wan.pp
+++ etc/puppet/manifests/config/wan.pp
@@ -1,6 +1,6 @@
 class config::wan {
-	$ip_add = "210.14.147.93"
-	$gateway = "210.14.147.65"
+	$ip_add = "12.34.56.78"
+	$gateway = "12.34.56.65"
 	$prefix = "27"
 
 	$opened_port = {
--- etc/puppet/manifests/config/web.pp
+++ etc/puppet/manifests/config/web.pp
@@ -1,4 +1,4 @@
 class config::web {
-	$public_domain_name = "wuzhuangbu.net"
+	$public_domain_name = "your.domain.com"
 	$www_root = "/var/www/lotusec/web_entrance"
 }
