e5d70a3bc36378641647e563774bbdec6051c5e1
qinjx <talkativedoggy@gmail.com>
2013-06-20 16:10:01 +0800

    mysql health monitor for haproxy

--- etc/puppet/modules/percona/manifests/cluster/install.pp
+++ etc/puppet/modules/percona/manifests/cluster/install.pp
@@ -3,7 +3,14 @@ class percona::cluster::install {
 		"disable_selinux":
 			lens => "Shellvars.lns",
 			incl => "/etc/selinux/config",
-			changes => "set SELINUX disabled",
+			changes => "set SELINUX disabled";
+		"add_galerachk_service":
+			context => "/files/etc/services",
+			changes => [
+				"set /files/etc/services/service-name[. = 'galerachk'] galerachk",
+				"set /files/etc/services/service-name[. = 'galerachk']/port 9200",
+				"set /files/etc/services/service-name[. = 'galerachk']/protocol tcp"
+			];
 	}
 
 	exec {
@@ -23,43 +30,42 @@ class percona::cluster::install {
 	}
 
 	file {
-		"/usr/bin/mysqlchk":
-			mode => 755,
-			user => nobody,
-			group => nobody,
-		    content => "MYSQLHOST=\"localhost\"
+		"/usr/bin/galerachk":
+				mode => 755,
+		    content => "#!/bin/sh
+MYSQLHOST=\"localhost\"
 MYSQLPORT=\"3306\"
 MYSQLUSERNAME=\"mgchkusr\"
 MYSQLPASSWORD=\"secret\"
  
-MYSQLSTATUS1=`mysql --force --host=$MYSQLHOST --port=$MYSQLPORT --user=$MYSQLUSERNAME --password=$MYSQLPASSWORD -B -N -e \"SHOW STATUS WHERE Variable_name = 'wsrep_ready';\" | awk '{print $2}'`
+MYSQLSTATUS1=`mysql --force --host=\$MYSQLHOST --port=\$MYSQLPORT --user=\$MYSQLUSERNAME --password=\$MYSQLPASSWORD -B -N -e \"SHOW STATUS WHERE Variable_name = 'wsrep_ready';\" | awk '{print \$2}'`
  
-if [ $MYSQLSTATUS1 = \"ON\" ]; then
-   MYSQLSTATUS2=`mysql --force --host=$MYSQLHOST --port=$MYSQLPORT --user=$MYSQLUSERNAME --password=$MYSQLPASSWORD -B -N -e \"SHOW STATUS WHERE Variable_name = 'wsrep_local_state_comment';\" | awk '{print $2}'`
-   if [ $MYSQLSTATUS2 = \"Synced\" ]; then
-      echo -e \"HTTP/1.1 200 OK\r\nContent-Type: Content-Type: text/plain\r\n\r\nMySQL Galera is Working\r\n\r\n\"
+if [ \$MYSQLSTATUS1 = \"ON\" ]; then
+   MYSQLSTATUS2=`mysql --force --host=\$MYSQLHOST --port=\$MYSQLPORT --user=\$MYSQLUSERNAME --password=\$MYSQLPASSWORD -B -N -e \"SHOW STATUS WHERE Variable_name = 'wsrep_local_state_comment';\" | awk '{print \$2}'`
+   if [[ \$MYSQLSTATUS2 = \"Synced\" ]]; then
+      echo -e \"HTTP/1.1 200 OK\r\\nContent-Type: Content-Type: text/plain\r\\n\r\\nMySQL Galera is working well\r\\n\r\\n\"
    else
-      echo -e \"HTTP/1.1 503 Service Unavailable\r\nContent-Type: Content-Type: text/plain\r\n\r\nMySQL Galera has Failed.\r\n\r\n\"
+      echo -e \"HTTP/1.1 503 Service Unavailable\r\\nContent-Type: Content-Type: text/plain\r\\n\r\\nMySQL Galera not synced.\r\\n\r\\n\"
    fi
 else
-   echo -e \"HTTP/1.1 503 Service Unavailable\r\nContent-Type: Content-Type: text/plain\r\n\r\nMySQL Galera has Failed.\r\n\r\n\"
+   echo -e \"HTTP/1.1 503 Service Unavailable\r\\nContent-Type: Content-Type: text/plain\r\\n\r\\nMySQL Galera not ready.\r\\n\r\\n\"
 fi";
-		"/etc/xinetd.d/mysqlchk":
+		"/etc/xinetd.d/galerachk":
 			mode => 755,
-			content => "service mysqlchk
+			content => "service galerachk
 {
         flags           = REUSE
         socket_type     = stream
         port            = 9200
         wait            = no
         user            = nobody
-        server          = /usr/bin/mysqlchk
+        server          = /usr/bin/galerachk
         log_on_failure  += USERID
         disable         = no
         only_from       = 0.0.0.0/0
         per_source      = UNLIMITED
 }",
-			require => File["/usr/bin/mysqlchk"],
+			require => [File["/usr/bin/galerachk"], Augeas["add_galerachk_service"]],
 			notify => Service["xinetd"];
 	}
 }
