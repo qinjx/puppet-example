4efd873016946685399cc412146a4f16ffb16857
qinjx <talkativedoggy@gmail.com>
2013-07-30 10:12:04 +0800

    init scripts放入vmtpl中

--- etc/puppet/manifests/node/default.pp
+++ etc/puppet/manifests/node/default.pp
@@ -1,26 +1,4 @@
 node default {
-	file {
-		"/root/init_scripts/":
-			ensure => directory;
-
-		"/root/init_scripts/conf.ini":
-			require => File["/root/init_scripts/"];
-		
-		"/root/init_scripts/init.sh":
-			source => "puppet:///files/node/default/init.sh";
-	}
-
-	augeas {
-		"init_vm_config_ini":
-			lens => "Shellvars.lns",
-			incl => "/root/init_scripts/conf.ini",
-			changes => [
-				"set puppet_server $config::global::host_puppet_server",
-				"set root_domain $config::global::root_domain_name"
-			],
-			require => File["/root/init_scripts/conf.ini"],
-	}
-
 	yum::repo::conf {
 		["centos_base", "centos_updates", "centos_extras", "epel"]:
 	}
@@ -29,15 +7,5 @@ node default {
 		$config::hosts::ip_keys:
 	}
 
-	puppet::client::conf {
-		"main/server":
-			ensure => "puppet-server.vip.$config::global::root_domain_name",
-	}
-
-	ntp::conf {
-		"server":
-			ensure => "puppet-server.vip",
-	}
-
-	include role_puppet_client, role_ssh_server, role_ntp_client
+	include role_ssh_server
 }
