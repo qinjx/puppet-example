25157396a6dd99348109a987855e353a05b01c28
qinjx <talkativedoggy@gmail.com>
2013-08-01 11:30:50 +0800

    bugfix: nfs::export need  para

--- etc/puppet/manifests/node/storage.dfs.pp
+++ etc/puppet/manifests/node/storage.dfs.pp
@@ -1,12 +1,12 @@
 node /^storage\d*\.dfs/ inherits default {
 	keepalived::vrrp::instance {
 		"${config::hosts::ip_list[nfs_sys_vip]}":
-		vip_add => "${config::global::ip_prefix}.${config::hosts::ip_list[nfs_sys_vip]}/24",
+			vip_add => "${config::global::ip_prefix}.${config::hosts::ip_list[nfs_sys_vip]}/24",
 	}
 
 	nfs::export {
 		"/mnt/fastdfs/upload_files":
-		    client => "${config::global::ip_prefix}.0/24",
+			client => "${config::global::ip_prefix}.${config::hosts::ip_list[nfs_sys_vip]}/24",
 	}
 
 	include role_fastdfs_storage, role_nfs_server, role_vip_holder, role_svn_server, role_internet_enduser
--- etc/puppet/modules/nfs/manifests/export.pp
+++ etc/puppet/modules/nfs/manifests/export.pp
@@ -1,8 +1,8 @@
-define nfs::export() {
+define nfs::export($client) {
     augeas {
-		"nfs_export_$name":
-		    context => "/files/etc/exports",
-		    changes => [
+		"nfs_export_${name}":
+			context => "/files/etc/exports",
+			changes => [
 				"set dir[.= '$name'] ${name}",
 				"set dir[.= '$name']/client ${client}",
 				"set dir[.= '$name']/client/option[1] ro",
