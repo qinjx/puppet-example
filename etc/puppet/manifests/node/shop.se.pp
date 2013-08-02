f5c30cce71bd6a5257ad8ab126b13a445ac99d45
qinjx <talkativedoggy@gmail.com>
2013-08-02 18:37:08 +0800

    搜索引擎可装包，但还不能自动运行服务

--- etc/puppet/manifests/node/shop.se.pp
+++ etc/puppet/manifests/node/shop.se.pp
@@ -1,8 +1,9 @@
 node /^shop\d*\.se/ inherits default {
 	file {
 		"/etc/sphinx/sphinx.conf":
-			require => Class["sphinx::install"],
-			source => "puppet:///files/node/shop.se/sphinx.conf",
+			source => "puppet:///files/node/shop.se/sphinx.conf";
+		"/etc/sphinx":
+			ensure => directory;
 	}
 
 	include role_search_engine
--- etc/puppet/manifests/role/search_engine.pp
+++ etc/puppet/manifests/role/search_engine.pp
@@ -1,4 +1,8 @@
 class role_search_engine {
+	yum::repo::conf {
+		"percona":
+	}
+
 	include sphinx::install, sphinx::service
 }
 
