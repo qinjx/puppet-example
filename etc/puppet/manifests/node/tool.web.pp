b0481031976b278b6640454c1123196107bd1848
qinjx <talkativedoggy@gmail.com>
2013-08-02 16:59:05 +0800

    tool.web恢复使用nginx，另寻hhvm测试机

--- etc/puppet/manifests/node/tool.web.pp
+++ etc/puppet/manifests/node/tool.web.pp
@@ -1,3 +1,14 @@
 node /^tool\d*\.web/ inherits default {
-	include role_hhvm_server
+	nginx::conf::vhost {
+		"tool.$config::web::public_domain_name":
+			www_root => "/var/www",
+			fcgi_conf => "php",
+	}
+
+	nginx::conf::location {
+		"php":
+			pattern => "\\.php\$",
+			fcgi_address => "127.0.0.1:9000",
+	}
+	include role_web_server, role_php_app_server
 }
