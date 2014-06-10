9b285a32c651f6b04d59ca257a0b0731c699202f
qinjx <talkativedoggy@gmail.com>
2014-06-10 23:18:21 +0800

    增加memcache扩展

--- etc/puppet/manifests/role/php_app_server.pp
+++ etc/puppet/manifests/role/php_app_server.pp
@@ -7,7 +7,7 @@ class role_php_app_server {
 	}
 
 	php::ext {
-		["bcmath", "gd", "mbstring", "mcrypt", "mysql", "pecl-apc", "pecl-imagick", "pecl-memcached", "pecl-mongo", "pecl-redis", "soap"]:
+		["bcmath", "gd", "mbstring", "mcrypt", "mysql", "pecl-apc", "pecl-imagick", "pecl-memcache", "pecl-memcached", "pecl-mongo", "pecl-redis", "soap"]:
 	}
 
 	php::conf::fpm {
