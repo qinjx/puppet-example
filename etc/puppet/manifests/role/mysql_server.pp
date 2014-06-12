e8df8e5f8393cd4c26fc1528924c2cbc8ffef8f0
qinjx <talkativedoggy@gmail.com>
2014-06-12 12:30:48 +0800

    更改mysql目录放到mysql::server::install中

--- etc/puppet/manifests/role/mysql_server.pp
+++ etc/puppet/manifests/role/mysql_server.pp
@@ -3,23 +3,5 @@ class role_mysql_server {
     "mysql":
   }
 
-  mysql::server::conf {
-    "datadir":
-      ensure => "/opt/data/mysql",
-      require => File["/opt/data/mysql"];
-  }
-
-  file {
-    "/opt/data/mysql":
-      ensure => directory,
-      require => Class["mysql::server::install"];
-  }
-
-  exec {
-    "change_mysqld_datadir":
-      command => "/etc/init.d/mysqld stop; /bin/cp -rp /var/lib/mysql /opt/data/; /etc/init.d/mysqld start",
-      require => Mysql::Server::Conf["datadir"];
-  }
-
   include mysql::server::install, mysql::server::service
 }
