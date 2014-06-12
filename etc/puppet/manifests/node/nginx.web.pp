95f2a043468db0325004393bce3d461e23d032dd
qinjx <talkativedoggy@gmail.com>
2014-06-12 15:40:46 +0800

    删除默认的nginx virtualhost

--- etc/puppet/manifests/node/nginx.web.pp
+++ etc/puppet/manifests/node/nginx.web.pp
@@ -1,16 +1,4 @@
 node /^nginx\d*\.web/ inherits default {
-	nginx::conf::vhost {
-		"acp.$config::web::public_domain_name":
-			www_root => "${config::web::www_root}/acp",
-			fcgi_conf => "acp-php",
-	}
-
-	nginx::conf::vhost {
-		"www.$config::web::public_domain_name":
-			www_root => "${config::web::www_root}/www",
-			fcgi_conf => "php",
-	}
-
 	nginx::conf::location {
 		"php":
 			pattern => "\\.php\$",
