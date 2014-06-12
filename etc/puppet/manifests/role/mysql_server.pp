01e48aecce3602c447dddfcfeb2872b44685c109
qinjx <talkativedoggy@gmail.com>
2014-06-12 11:43:26 +0800

    把/var/lib/mysql目录cp到/opt/data去

--- etc/puppet/manifests/role/mysql_server.pp
+++ etc/puppet/manifests/role/mysql_server.pp
@@ -14,5 +14,11 @@ class role_mysql_server {
       ensure => directory;
   }
 
+  exec {
+    "change_mysqld_datadir":
+      command => "/etc/init.d/mysqld stop; /bin/cp -rp /var/lib/mysql /opt/data/; /etc/init.d/mysqld start",
+      require => Mysql::Server::Conf["datadir"];
+  }
+
   include mysql::server::install, mysql::server::service
 }
