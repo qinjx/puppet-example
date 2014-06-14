e6225fc45bf6ba431ca241895ab17dbeb89727c9
qinjx <talkativedoggy@gmail.com>
2014-06-14 10:20:45 +0800

    php机器增加一个hosts

--- etc/puppet/manifests/node/php.app.pp
+++ etc/puppet/manifests/node/php.app.pp
@@ -1,3 +1,9 @@
 node /^php\d*\.app/ inherits default {
+  host {
+    $host_domain:
+    ip => "${config::global::ip_prefix}.${config::hosts::ip_list[wan_gw_vip]}",
+    name => "www.${config::web::public_domain_name}",
+  }
+
 	include role_php_app_server, role_nfs_client
 }
