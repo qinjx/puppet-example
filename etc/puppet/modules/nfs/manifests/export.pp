6e3b4c23620df2f1a3f6b7b8fddcc7b25c05838d
qinjx <talkativedoggy@gmail.com>
2014-06-14 09:21:35 +0800

    nfs export增加权限限制

--- etc/puppet/modules/nfs/manifests/export.pp
+++ etc/puppet/modules/nfs/manifests/export.pp
@@ -1,11 +1,11 @@
 define nfs::export($client) {
-    augeas {
+  augeas {
 		"nfs_export_${name}":
 			context => "/files/etc/exports",
 			changes => [
 				"set dir[.= '$name'] ${name}",
 				"set dir[.= '$name']/client ${client}",
-				"set dir[.= '$name']/client/option[1] ro",
+				"set dir[.= '$name']/client/option[1] ${permission}",
 				"set dir[.= '$name']/client/option[2] all_squash"
 			],
 			notify => Service["rpcbind"],
