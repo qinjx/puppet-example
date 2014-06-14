649e6f0182327ed6da46e2695842fea2e1c38678
qinjx <talkativedoggy@gmail.com>
2014-06-14 10:21:52 +0800

    bugfix

--- etc/puppet/manifests/node/php.app.pp
+++ etc/puppet/manifests/node/php.app.pp
@@ -1,6 +1,6 @@
 node /^php\d*\.app/ inherits default {
   host {
-    $host_domain:
+    "web_service_www":
     ip => "${config::global::ip_prefix}.${config::hosts::ip_list[wan_gw_vip]}",
     name => "www.${config::web::public_domain_name}",
   }
