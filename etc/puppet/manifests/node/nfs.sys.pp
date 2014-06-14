9ee803f9c479bbb4206f5103c14602119a3509b7
qinjx <talkativedoggy@gmail.com>
2014-06-14 09:22:10 +0800

    nfs服务器把upload目录export出去

--- etc/puppet/manifests/node/nfs.sys.pp
+++ etc/puppet/manifests/node/nfs.sys.pp
@@ -3,5 +3,18 @@ node /^nfs\d*\.sys/ inherits default {
     "${config::hosts::ip_list[nfs_sys_vip]}":
       vip_add => "${config::global::ip_prefix}.${config::hosts::ip_list[nfs_sys_vip]}/24",
   }
+
+  nfs::export {
+    "/opt/nfs/upload":
+    client => "${config::global::ip_prefix}.${config::hosts::ip_list[nfs_sys_vip]}/24",
+    permission => "rw",
+    require => File["/opt/nfs/upload"],
+  }
+
+  file {
+    "/opt/nfs/upload":
+      ensure => directory,
+  }
+
 	include role_vip_holder, role_nfs_server
 }
--- etc/puppet/manifests/node/storage.dfs.pp
+++ etc/puppet/manifests/node/storage.dfs.pp
@@ -7,6 +7,7 @@ node /^storage\d*\.dfs/ inherits default {
 	nfs::export {
 		"/mnt/fastdfs/upload_files":
 			client => "${config::global::ip_prefix}.${config::hosts::ip_list[nfs_sys_vip]}/24",
+      permission => "ro",
 	}
 
 	include role_fastdfs_storage, role_nfs_server, role_vip_holder, role_svn_server, role_internet_enduser
