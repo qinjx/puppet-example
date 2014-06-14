842af4d44b78f2f5a442385526b7e239c99e8cf5
qinjx <talkativedoggy@gmail.com>
2014-06-14 09:14:29 +0800

    nfs服务器使用vip

--- etc/puppet/manifests/node/nfs.sys.pp
+++ etc/puppet/manifests/node/nfs.sys.pp
@@ -1,3 +1,7 @@
 node /^nfs\d*\.sys/ inherits default {
-	include role_nfs_server
+  keepalived::vrrp::instance {
+    "${config::hosts::ip_list[nfs_sys_vip]}":
+      vip_add => "${config::global::ip_prefix}.${config::hosts::ip_list[nfs_sys_vip]}/24",
+  }
+	include role_vip_holder, role_nfs_server
 }
