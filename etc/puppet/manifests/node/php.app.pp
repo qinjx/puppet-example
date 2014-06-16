c52c36962b10a5e4a1dd557ae728140716782d72
qinjx <talkativedoggy@gmail.com>
2014-06-16 11:49:24 +0800

    增加两个hosts

--- etc/puppet/manifests/node/php.app.pp
+++ etc/puppet/manifests/node/php.app.pp
@@ -2,7 +2,15 @@ node /^php\d*\.app/ inherits default {
   host {
     "web_service_www":
     ip => "${config::global::ip_prefix}.${config::hosts::ip_list[wan_gw_vip]}",
-    name => "www.${config::web::public_domain_name}",
+    name => "www.${config::web::public_domain_name}";
+
+    "user_service_www":
+    ip => "${config::global::ip_prefix}.${config::hosts::ip_list[wan_gw_vip]}",
+    name => "user.${config::web::public_domain_name}";
+
+    "new_service_www":
+    ip => "${config::global::ip_prefix}.${config::hosts::ip_list[wan_gw_vip]}",
+    name => "new.${config::web::public_domain_name}";
   }
 
 	include role_php_app_server, role_nfs_client
