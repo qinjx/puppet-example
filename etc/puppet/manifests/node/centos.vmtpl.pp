741a44fdbcc7177a0fb0da2a0784d4870cad0ede
qinjx <talkativedoggy@gmail.com>
2014-06-23 21:26:25 +0800

    centos vmtpl remove role_ntp_client

--- etc/puppet/manifests/node/centos.vmtpl.pp
+++ etc/puppet/manifests/node/centos.vmtpl.pp
@@ -4,10 +4,5 @@ node /^centos\d*\.vmtpl/ inherits default {
 		ensure => "puppet-server.vip.$config::global::root_domain_name",
 	}
 
-	ntp::conf {
-		"server":
-		ensure => "puppet-server.vip",
-	}
-
-	include role_centos_vmtpl, role_puppet_client, role_ntp_client
+	include role_centos_vmtpl, role_puppet_client
 }
