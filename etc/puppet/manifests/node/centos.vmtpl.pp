92c990a3bcd50c95a7d4165966f4007af442ece0
qinjx <talkativedoggy@gmail.com>
2014-06-23 14:41:43 +0800

    初始化conf.ini也放到default node

--- etc/puppet/manifests/node/centos.vmtpl.pp
+++ etc/puppet/manifests/node/centos.vmtpl.pp
@@ -9,16 +9,5 @@ node /^centos\d*\.vmtpl/ inherits default {
 		ensure => "puppet-server.vip",
 	}
 
-	augeas {
-		"init_vm_config_ini":
-		lens => "Shellvars.lns",
-		incl => "/root/init_scripts/conf.ini",
-		changes => [
-		"set puppet_server $config::global::host_puppet_server",
-		"set root_domain $config::global::root_domain_name"
-		],
-		require => File["/root/init_scripts/conf.ini"],
-	}
-
 	include role_centos_vmtpl, role_puppet_client, role_ntp_client
 }
--- etc/puppet/manifests/node/default.pp
+++ etc/puppet/manifests/node/default.pp
@@ -9,13 +9,24 @@ node default {
 
 	file {
 		"/root/init_scripts/":
-		ensure => directory;
+			ensure => directory;
 
 		"/root/init_scripts/conf.ini":
-		require => File["/root/init_scripts/"];
+			require => File["/root/init_scripts/"];
 
 		"/root/init_scripts/init.sh":
-		source => "puppet:///files/node/default/init.sh";
+			source => "puppet:///files/node/default/init.sh";
+	}
+
+	augeas {
+		"init_vm_config_ini":
+			lens => "Shellvars.lns",
+			incl => "/root/init_scripts/conf.ini",
+			changes => [
+				"set puppet_server $config::global::host_puppet_server",
+				"set root_domain $config::global::root_domain_name"
+			],
+			require => File["/root/init_scripts/conf.ini"],
 	}
 
 	include role_ssh_server
