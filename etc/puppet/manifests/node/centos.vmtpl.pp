b0d392732c66aeb232cbbe1343a1c43d450cd172
qinjx <talkativedoggy@gmail.com>
2014-06-24 16:49:39 +0800

    删除role_centos_vmtpl

--- etc/puppet/manifests/node/centos.vmtpl.pp
+++
@@ -1,8 +0,0 @@
-node /^centos\d*\.vmtpl/ inherits default {
-	puppet::client::conf {
-		"main/server":
-		ensure => "puppet-server.vip.$config::global::root_domain_name",
-	}
-
-	include role_centos_vmtpl, role_puppet_client
-}
