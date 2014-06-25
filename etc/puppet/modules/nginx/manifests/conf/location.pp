15b3792ecb001d31c2d60df2b93e5fc8b6aebfb3
qinjx <talkativedoggy@gmail.com>
2014-06-25 17:50:28 +0800

    nginx 1.6没有fastcgi.conf了，兼容之

--- etc/puppet/modules/nginx/manifests/conf/location.pp
+++ etc/puppet/modules/nginx/manifests/conf/location.pp
@@ -2,7 +2,7 @@ define nginx::conf::location($pattern, $fcgi_address) {
 	file {
 		"/etc/nginx/fastcgi_${name}.conf":
 			content => "location ~ $pattern {
-	include fastcgi.conf;
+	include fastcgi_params;
 	fastcgi_pass $fcgi_address;
 }",
 			require => Package["nginx"],
