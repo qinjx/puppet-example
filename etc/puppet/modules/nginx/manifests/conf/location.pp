cbd4f88254c586019c869f9420f782391eead6cf
qinjx <talkativedoggy@gmail.com>
2014-06-25 17:50:57 +0800

    自行在nginx.conf中添加SCRIPT_FILENAME

--- etc/puppet/modules/nginx/manifests/conf/location.pp
+++ etc/puppet/modules/nginx/manifests/conf/location.pp
@@ -2,6 +2,8 @@ define nginx::conf::location($pattern, $fcgi_address) {
 	file {
 		"/etc/nginx/fastcgi_${name}.conf":
 			content => "location ~ $pattern {
+
+	fastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;
 	include fastcgi_params;
 	fastcgi_pass $fcgi_address;
 }",
