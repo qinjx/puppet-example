73b56f68194d69c3d7d38cab2edc390be16fb79d
qinjx <talkativedoggy@gmail.com>
2014-07-03 14:38:52 +0800

    haproxy配置更新时重启service，add server也发notify

--- etc/puppet/modules/haproxy/manifests/conf/server.pp
+++ etc/puppet/modules/haproxy/manifests/conf/server.pp
@@ -13,6 +13,7 @@ define haproxy::conf::server($port, $cluster, $check_port = nil) {
 		"add_server_$name":
 			command => "echo '	server	$name	$name:$port	check $check_string' >> /etc/haproxy/conf.d/$cluster.cfg",
 			path => "/bin",
+			notify => Service["haproxy"],
 			require => Exec["delete_if_exists_server_$name"];
 	}
 }
