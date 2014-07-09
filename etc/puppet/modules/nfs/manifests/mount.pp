67f73e9f6990f163762001497bf8ab9f2601ee99
qinjx <talkativedoggy@gmail.com>
2014-07-09 16:25:20 +0800

    write fstab before mount -a

--- etc/puppet/modules/nfs/manifests/mount.pp
+++ etc/puppet/modules/nfs/manifests/mount.pp
@@ -6,8 +6,9 @@ define nfs::mount($dev, $mount_point = $name) {
 		"write_fstab-$dev-$mount_point":,
 			command => "echo '$dev		$mount_point		nfs	defaults	0 0' >> /etc/fstab",
 			unless => "grep '$dev' /etc/fstab";
-		"refresh_fstab_for_-$mount_point":
+		"refresh_fstab_for-$mount_point":
 			command => "mount -a",
-			unless => "df | grep '$mount_point'";
+			unless => "df | grep '$mount_point'",
+			require => Exec["write_fstab-$dev-$mount_point"];
 	}
 }
