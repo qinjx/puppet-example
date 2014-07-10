009f251db8e9bb014a3c5458e3f192337874a0ef
qinjx <talkativedoggy@gmail.com>
2014-07-10 23:47:06 +0800

    先装mysqld再修改配置

--- etc/puppet/modules/mysql/manifests/server/conf.pp
+++ etc/puppet/modules/mysql/manifests/server/conf.pp
@@ -3,6 +3,7 @@ define mysql::server::conf($ensure) {
 		"change_mysqld_setting_${name}":
 			context => "/files/etc/my.cnf/target[ . = 'mysqld' ]",
 			changes => "set ${name} ${ensure}",
+			require => Package["mysql-community-server"],
 			notify => Service["mysqld"],
 	}
 }
