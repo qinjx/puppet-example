0984f09c05f25797baa683c80a4d80c373de4d42
qinjx <talkativedoggy@gmail.com>
2014-07-15 17:46:08 +0800

    nginx默认开启gzip

--- etc/puppet/modules/nginx/manifests/install.pp
+++ etc/puppet/modules/nginx/manifests/install.pp
@@ -6,4 +6,10 @@ class nginx::install {
 	firewall::filter::allow {
 		"80":
 	}
+
+	exec {
+		"enable_gzip_by_default":
+			command => "sed -i \"s/#gzip/gzip/\" /etc/nginx/nginx.conf",
+			require => Package["nginx"],
+	}
 }
