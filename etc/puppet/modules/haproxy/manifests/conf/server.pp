76f970ac9e972a8e1d9137160e8a045d29df5a72
qinjx <talkativedoggy@gmail.com>
2014-07-16 19:56:36 +0800

    haproxy server 依赖haproxy cluster

--- etc/puppet/modules/haproxy/manifests/conf/server.pp
+++ etc/puppet/modules/haproxy/manifests/conf/server.pp
@@ -1,17 +1,17 @@
-define haproxy::conf::server($port, $cluster, $check_port = nil) {
+define haproxy::conf::server($port, $cluster, $check_port = nil, $server = $name) {
 	if ($check_port == nil) {
 		$check_string = ""
 	} else {
 		$check_string = "port $check_port"
 	}
 
-	$line = "	server  $name   $name:$port     check $check_string"
+	$line = "	server  $server   $server:$port     check $check_string"
 	$file = "/etc/haproxy/conf.d/$cluster.cfg"
 	exec {
-		"add_server_$name":
+		"add_server_$server:$cluster":
 			command => "echo '$line' >> $file",
 			notify => Service["haproxy"],
-			onlyif => "test `grep '$name:$port' $file | wc | awk '{print \$1}'` -eq 0",
-			require => File["/etc/haproxy/conf.d"],
+			onlyif => "test `grep '$server:$port' $file | wc | awk '{print \$1}'` -eq 0",
+			require => Haproxy::Conf::Cluster[$cluster],
 	}
 }
