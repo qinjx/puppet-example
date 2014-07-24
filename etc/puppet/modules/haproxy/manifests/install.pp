97d5a38f7dd3cf58dc70e2fbaab534b6bdd788bb
qinjx <talkativedoggy@gmail.com>
2014-07-24 18:31:20 +0800

    haproxy默认maxconn提高10倍

--- etc/puppet/modules/haproxy/manifests/install.pp
+++ etc/puppet/modules/haproxy/manifests/install.pp
@@ -19,28 +19,32 @@ class haproxy::install {
 	exec {#多个配置文件要指定多次-f参数
 		"inert_conf_array_line":
 			command => 'sed -i "/prog=/ a conf_array=\`find /etc/haproxy -name \'*.cfg\' | sort -r | awk \'{print \"-f \", \$1}\'\`" /etc/init.d/haproxy',
-			unless => "grep conf_array= /etc/init.d/haproxy 2>/dev/null",
-			path => "/bin",
+			unless => "grep conf_array= /etc/init.d/haproxy",
 			require => Package["haproxy"];
 
 		"enable_multi_config_file_for_haproxy":
 			command => 'sed -i "s/\-f \/etc\/\$prog\/\$prog\.cfg/\$\{conf_array\[@\]\}/g" /etc/init.d/haproxy',
-			path => "/bin",
+			unless => "grep 'conf_array\[@' /etc/init.d/haproxy",
 			require => Package["haproxy"];
 
 		"truncate_default_haproxy_cfg":
 			command => "sed -i '/main/,\$d' /etc/haproxy/haproxy.cfg",
-			path => "/bin",
+			onlyif => "grep main /etc/haproxy/haproxy.cfg",
                         require => Package["haproxy"];
 
 		"set_default_mode_tcp":
 			command => "sed -i \"s/http\$/tcp/\" /etc/haproxy/haproxy.cfg",
-			path => "/bin",
+			unless => "grep mode /etc/haproxy/haproxy.cfg | grep tcp",
 			require => Package["haproxy"];
 
 		"delete_http_option":
 			command => "sed -i \"/httplog\|forwardfor\|http-server-close/d\" /etc/haproxy/haproxy.cfg",
-                        path => "/bin",
+			onlyif => "grep 'httplog\|forwardfor\|http-server-close' /etc/haproxy/haproxy.cfg",
                         require => Package["haproxy"];
+
+		"set_maxconn":
+			command => "sed -i 's/maxconn\( \+[0-9]\+\)/maxconn\10/' /etc/haproxy/haproxy.cfg",
+                        onlyif => "grep maxconn /etc/haproxy/haproxy.cfg | grep 3000\$",
+                        require => Package["haproxy"]
 	}
 }
