a962e557b9e9d59d6061ac73c288df816fafa300
qinjx <talkativedoggy@gmail.com>
2014-07-26 19:09:10 +0800

    ssh免登录模块

---
+++ etc/puppet/modules/ssh/manifests/key/sendto.pp
@@ -0,0 +1,15 @@
+class ssh::key::sendto() {
+	$key_file = "~/.ssh/id_rsa.pub"
+	exec {
+		"auto_generate_ssh_key":
+			command => 'ssh-keygen -f $key_file -q -N ""',
+                        unless => "ls $key_file";
+	}
+
+	file {
+		"/root/.ssh/send_key_to.sh":
+			content => "#!/bin/bash
+scp $key_file \$1:/tmp/$hostname
+ssh \$1 'cat /tmp/$hostname >> ~/.ssh/authorized_keys; rm /tmp/$hostname'",
+	}
+}
